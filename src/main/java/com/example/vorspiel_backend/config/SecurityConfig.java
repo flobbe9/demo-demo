429f64d758dc9c232a446e149eb6815bb7f6edd4
flobbe9 <florin735@live.com>
2023-09-09 14:34:02 +0200

    docs: add global cors
    add .env
    
    add dev docker-compose

--- src/main/java/com/example/vorspiel_backend/config/SecurityConfig.java
+++ src/main/java/com/example/vorspiel_backend/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.vorspiel_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 /**
@@ -17,10 +20,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity // for @PreAuthorize
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${FRONTEND_BASE_URL}")
+    private String frontendBaseUrl;
+
     
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // security
         http
             .csrf(csrf -> csrf
                 .disable())
@@ -30,5 +38,24 @@ public class SecurityConfig {
             );
 
         return http.build();
+    }
+
+
+    /**
+     * Allow frontend url with any pattern.
+     * 
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(frontendBaseUrl)
+                        .allowedMethods("GET", "POST", "UPDATE", "DELETE");
+            }
+        };
     }
 }
--- src/main/java/com/example/vorspiel_backend/controllers/TestController.java
+++ src/main/java/com/example/vorspiel_backend/controllers/TestController.java
@@ -19,7 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,7 +43,6 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @RequestMapping("/test")
 @Validated
-@CrossOrigin
 @Log4j2
 public class TestController {
 
