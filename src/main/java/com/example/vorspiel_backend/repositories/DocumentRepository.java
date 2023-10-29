cc049e32bd279a703716ec54153e0c283c0874e7
flobbe9 <florin735@live.com>
2023-10-29 12:06:05 +0100

    feat!: add document entity

---
+++ src/main/java/com/example/vorspiel_backend/repositories/DocumentRepository.java
@@ -0,0 +1,11 @@
+package com.example.vorspiel_backend.repositories;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.vorspiel_backend.entites.Document;
+
+
+@Repository
+public interface DocumentRepository extends Dao<Document> {
+    
+}
