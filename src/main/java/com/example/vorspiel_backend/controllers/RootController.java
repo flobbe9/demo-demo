d6cb44d3ffcaca73b76916067324e97e29f981db
f.schikarski <f.schikarski@gingco.de>
2024-01-10 13:11:16 +0100

    feat: add version endpoint

--- src/main/java/com/example/vorspiel_backend/controllers/RootController.java
+++ src/main/java/com/example/vorspiel_backend/controllers/RootController.java
@@ -1,10 +1,5 @@
 package com.example.vorspiel_backend.controllers;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,42 +11,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 /**
- * Class handling http requests for root path '/'
+ * Class handling http requests for root path '/'.
+ * 
+ * @since 0.0.5
  */
 @RequestMapping("/")
 @RestController
-@Tag(name = "Root controller")
+@Tag(name = "Root endpoints")
 public class RootController {
     
     @GetMapping("/version")
     @Operation(summary = "View api version.")
     public String getVersion() {
 
-        return getApiVersion();
-    }
-
-
-    /**
-     * @return the version of this api from {@code build.gradle} or and empty String if 'version' prop is not found
-     */
-    private static String getApiVersion() {
-
-        String fileName = "build.gradle";
-        String propName = "version";
-
-        try (InputStream in = new FileInputStream(fileName)) {
-            Properties props = new Properties();
-            props.load(in);
-            Object versionProp = props.get(propName);
-
-            String version = versionProp != null ? versionProp.toString().replace("'", "") 
-                                                 : "Failed to get version. Could not find '" + propName + "' attribute in '" + fileName + "' file.";
-
-            return version;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "Failed to get version. Could not find file '" + fileName + "'";
-        }
+        return VorspielApplication.getApiVersion();
     }
 }
