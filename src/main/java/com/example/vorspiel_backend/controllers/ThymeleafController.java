f3477fd3849cd438390724d64fdbe2b21b496c3e
f.schikarski <f.schikarski@gingco.de>
2024-01-10 11:47:47 +0100

    feat: add thymeleaf controller improove index.html

---
+++ src/main/java/com/example/vorspiel_backend/controllers/ThymeleafController.java
@@ -0,0 +1,48 @@
+package com.example.vorspiel_backend.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.vorspiel_backend.VorspielApplication;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+
+/**
+ * Class handling endpoints using html pages in '/resources/templates' folder.
+ * 
+ * @since 0.0.5
+ */
+@Controller
+@Tag(name = "Html template endpoints")
+public class ThymeleafController {
+
+    @Value("${WEBSITE_NAME}")
+    private String websiteName;
+
+    @Value("${API_NAME}")
+    private String apiName;
+
+    @Value("${BASE_URL}")
+    private String baseUrl;
+
+    @Value("${DB_VERSION}")
+    private String dbVersion;
+
+    
+    @GetMapping("/")
+    @Operation(summary = "View basic information about api.")
+    public String getIndex(Model model) {
+
+        model.addAttribute("WEBSITE_NAME", this.websiteName);
+        model.addAttribute("API_NAME", this.apiName);
+        model.addAttribute("BASE_URL", this.baseUrl);
+        model.addAttribute("API_VERSION", VorspielApplication.getApiVersion());
+        model.addAttribute("DB_VERSION", this.dbVersion);
+
+        return "index";
+    }
+}
